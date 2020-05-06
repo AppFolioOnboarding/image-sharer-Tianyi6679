f0c5f1d09589ca3851fe840e95e70be1fa6108b6
Tianyi6679 <timtianyima@gmail.com>
2020-05-06 13:52:13 -0700

    rubocop again

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "root welcome#index" do
+  test 'root welcome#index' do
     get root_url
-    assert_equal "index", @controller.action_name
+    assert_equal 'index', @controller.action_name
     assert_response :success
-    assert_match "Welcome", @response.body
+    assert_match 'Welcome', @response.body
   end
 end
