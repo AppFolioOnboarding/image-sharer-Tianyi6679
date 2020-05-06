651ccbecbd1b2670cf8c7691251a5daae064a461
Tianyi6679 <timtianyima@gmail.com>
2020-05-06 13:48:42 -0700

    add test for root path action

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "root welcome#index" do
+    get root_url
+    assert_equal "index", @controller.action_name
+    assert_response :success
+    assert_match "Welcome", @response.body
+  end
 end
