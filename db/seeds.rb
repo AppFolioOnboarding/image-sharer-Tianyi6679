d1115a02b3474228a8a5cd6f44b9938b857542f4
Tianyi6679 <timtianyima@gmail.com>
2020-05-11 11:26:12 -0700

    add image seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Image.create!([
+               {title: 'dog_img1', url: 'https://cdn.mos.cms.futurecdn.net/JzqhuEDTRfCZKMKHUxPySB.jpg'},
+               {title: 'dog_img2', url: 'https://dogtime.com/assets/uploads/2012/05/dog-bathing-e1571421548676-1280x720.jpg'},
+               {title: 'dog_img3', url: 'https://scx1.b-cdn.net/csz/news/800/2019/2-dog.jpg'},
+               {title: 'dog_img4', url: 'https://api.time.com/wp-content/uploads/2020/04/dog-coronavirus.jpg'},
+               {title: 'dog_img5', url: 'https://www.purina.co.uk/sites/g/files/mcldtz2481/files/2018-09/shutterstock_205844974.jpg'},
+               {title: 'dog_img6', url: 'https://cdn.sanity.io/images/0vv8moc6/dvm360/81e9bbc1fe445afd4c888497d6e8e4d8abcd9029-450x274.jpg'},
+               {title: 'dog_img7', url: 'https://www.helpguide.org/wp-content/uploads/woman-cradling-dog.jpg'},
+               {title: 'dog_img8', url: 'https://www.rover.com/blog/wp-content/uploads/2019/03/frenchie-puppy-gazinf-pixabay.jpg'},
+               {title: 'dog_img9', url: 'https://www.justfoodfordogs.com/content/images/thumbs/0000977.jpeg'},
+               {title: 'dog_img10', url: 'https://s01.sgp1.cdn.digitaloceanspaces.com/article/131928-mxiccwtarv-1575034997.jpg'},
+               {title: 'dog_img11', url: 'https://static.wixstatic.com/media/f1b7c5_bff1477eaeb24078a8a590249f8d05b9~mv2_d_3348_2587_s_4_2.jpg'},
+               {title: 'dog_img12', url: 'https://www.rocketdogrescue.org/wp-content/uploads/2020/04/thumbnail-1067-600x564.jpeg'},
+               {title: 'dog_img13', url: 'https://embarkvet.com/wp-content/uploads/2020/04/dna_blue_mobile.jpg'},
+               {title: 'dog_img14', url: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/167C3/production/_111699029_gettyimages-1168451046.jpg'},
+               {title: 'dog_img17', url: 'https://www.healthline.com/hlcmsresource/images/Dog-Breeds-Health-Problems/3180-Pug_green_grass-1200x628-FACEBOOK.jpg'},
+               {title: 'cat_img18', url: 'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B1189%2C24%5D&w=1983&h=1038&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F03%2FOrange-cat-names.jpg'},
+               {title: 'cat_img19', url: 'https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697'},
+               {title: 'cat_img20', url: 'https://postmediatorontosun.files.wordpress.com/2019/12/cat-e1575303121192.jpg'},
+             ])
