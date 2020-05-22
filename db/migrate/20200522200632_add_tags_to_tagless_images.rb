082c5a4564cc67d2e3bbf9a3c42a31be82c26df2
Tianyi Ma <tianyi.ma@appfolio.com>
2020-05-22 13:58:12 -0700

    rubocop

--- db/migrate/20200522200632_add_tags_to_tagless_images.rb
+++ db/migrate/20200522200632_add_tags_to_tagless_images.rb
@@ -1,6 +1,6 @@
 class AddTagsToTaglessImages < ActiveRecord::Migration[5.2]
   def change
-    Image.all.filter {|img| img.tag_list.empty?}.each do |img|
+    Image.all.filter { |img| img.tag_list.empty? }.each do |img|
       img.tag_list.add('please_add_tag')
       img.save!
     end
